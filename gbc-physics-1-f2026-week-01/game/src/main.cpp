ce12a46b9ff1aa43812d672850f832d27b6751e4
RomuluStudios <kirbymarshall94@gmail.com>
2026-09-10 15:02:07 -0400

    Week 1
    All TODO finished except for animation of Purple circle.
    
    Added TODO for making time display 2 decimals

---
+++ gbc-physics-1-f2026-week-01/game/src/main.cpp
@@ -0,0 +1,60 @@
+#include "raylib.h"
+#include "raymath.h"
+#include "raygui.h"
+#include "cstdio"
+int main()
+{
+    InitWindow(800, 800, "Physics-1");
+    InitAudioDevice();
+    SetTargetFPS(60);
+
+    float x = 400.0f;
+    float y = 400.0f;
+    float a = 10.0f;    // amplitude (radius of movement)
+    float b = 4.0f;     // frequency (rate of movement)
+
+    Vector2 pos = { 100.0f, 400.0f };
+
+    while (!WindowShouldClose())
+    {
+        float dt = GetFrameTime();
+        float t = GetTime();
+        printf("%f\n", t);
+        //TraceLog(LOG_INFO, "%f", time); // raylib uses tracelog to output to the console
+        //TraceLog(LOG_INFO, "%f", dt);
+
+        // Simple animation
+        pos += Vector2UnitX * 100.0f * dt;
+
+        BeginDrawing();
+            ClearBackground(WHITE);
+
+            DrawCircleV(pos, 20.0f, RED);
+
+            DrawCircle(50, 100, 25.0f, GREEN);
+            DrawCircle(150, 200, 25.0f, DARKGREEN);
+            DrawLine(50, 100, 150, 200, LIME);
+
+            DrawCircleV(GetMousePosition(), 20.0f, RED);
+            DrawText("Kirby Marshall 101554268", 540, 750, 20, ORANGE);
+            // If you want to be very accurate with formatting, use the MeasureText function!
+            //MeasureText("Connor Smiley 123456", 20);
+
+            // Use TextFormat to convert data (like int or float) to const char* (text)!
+            // TextFormat follows C-style formatting: https://cplusplus.com/reference/cstdio/printf/
+
+            const char* text = TextFormat("Time: %f", t);
+            //TODO -- make time 2 decimals
+            DrawText(text, 650, 5, 20, BLUE);
+            
+
+            // TODO -- animate this based on equation in lab document week 1!
+            DrawCircle(x, y, 25.0f, PURPLE);
+
+        EndDrawing();
+    }
+
+    CloseAudioDevice();
+    CloseWindow();
+    return 0;
+}
